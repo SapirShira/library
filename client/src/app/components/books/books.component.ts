3112a75df04146f39b03d03de0bc512273152fa8
SapirShira <shirahalili78@gmail.com>
2020-06-08 15:22:03 +0300

    change in books component

--- client/src/app/components/books/books.component.ts
+++ client/src/app/components/books/books.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {MatCalendarCellCssClasses} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent implements OnInit{
+
+  dateClass = (d: Date): MatCalendarCellCssClasses => {
+    const date = d.getDate();
+
+    // Highlight the 1st and 20th day of each month.
+    return (date === 1 || date === 20) ? 'example-custom-date-class' : '';
+  }
 
   constructor() { }
 
